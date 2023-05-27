1b584164b0799b4024bc1f30725bd415175e7a0d
iosip <iosipoi@gmail.com>
2023-05-27 09:23:31 +0300

    SOLID: add interface segregation

---
+++ SOLID/InterfaceSegregationPrinciple.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    public interface ILead 
+    {
+        void CreateSubTask();
+        void AssignTask();
+    }
+
+    public interface IProgrammer
+    {
+        void WorkOnTask();
+    }
+    public class TeamLead : ILead, IProgrammer
+    {
+        public void AssignTask()
+        {
+            //asigneaza task
+        }
+
+        public void CreateSubTask()
+        {
+            //creaza task
+        }
+
+        public void WorkOnTask()
+        {
+            //lucreaza pe task
+        }
+    }
+
+    public class Manager : ILead
+    {
+        public void AssignTask()
+        {
+            //
+        }
+
+        public void CreateSubTask()
+        {
+            //
+        }
+    }
+}

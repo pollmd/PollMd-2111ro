aac57e58cb6546cca93d3f60e11518dae958e8db
Yoga Premium <Yoga Premium@ATIK-LAPTOP>
2023-06-10 11:38:29 +0300

    init

--- PollMd2/ClientApp/src/components/Poll.js
+++ PollMd2/ClientApp/src/components/Poll.js
@@ -3,56 +3,57 @@ import ProgressBar from 'react-bootstrap/ProgressBar';
 import { LinksGrid } from './LinksGrid';
 
 export class Poll extends Component {
-  static displayName = Poll.name;
+    static displayName = Poll.name;
 
-  constructor(props) {
-    super(props);
-    this.state = { polls: [], loading: true };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            polls: [],
+            loading: true,
+        };
+    }
 
-  componentDidMount() {
-    this.populatePollData();
-  }
+    componentDidMount() {
+        this.populatePollData();
+    }
 
-  static renderPollsForm(polls) {
-      return (
-          <>
-            <h1>{polls.text}</h1>
-              <iframe id="iframep" name="iframep" style={{ display: 'none' }} ></iframe>
-            <form action="questions/vote" method="POST" target="iframep">
-            {polls.answers.map(answ =>
-                <div key={answ.id}>
-                    <input type="radio" value={answ.id} name="optionid" />
-                    <label>{answ.text} - <i>{ answ.votes }</i></label>
-                    <ProgressBar striped variant="info" now={answ.votes} />
-                </div>
-                  )}
-                <br />
-                  <input type="submit" />
-              </form>
-              <LinksGrid/>
-        </>
-    );
-  }
-
-  render() {
-    let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-        : Poll.renderPollsForm(this.state.polls);
-
-    return (
-      <div>
-        {contents}
-      </div>
-    );
-  }
+    static renderPollsForm(polls) {
+        return (
+            <>
+                <h1>{polls.text}</h1>
+                <iframe id="iframep" name="iframep" style={{ display: 'none' }}></iframe>
+                <form action="questions/vote" method="POST" target="iframep">
+                    {polls.answers.map((answ) => (
+                        <div key={answ.id}>
+                            <input type="radio" value={answ.id} name="optionid" />
+                            <label>
+                                {answ.text} - <i>{answ.votes}</i>
+                            </label>
+                            <ProgressBar striped variant="info" now={answ.votes} />
+                        </div>
+                    ))}
+                    <br />
+                    <input type="submit" />
+                </form>
+                <LinksGrid />
+            </>
+        );
+    }
 
     async populatePollData() {
         const params = new URLSearchParams(window.location.search);
-        let pollId = params.get("id")
-        if(pollId==null) pollId=1
+        let pollId = params.get('id');
+        if (pollId == null) pollId = 1;
+
+        const submittedQuestions = params.get('questions');
+        let questionsArray = [];
+        if (submittedQuestions) {
+            questionsArray = JSON.parse(submittedQuestions);
+        }
+
         const response = await fetch('questions?id=' + pollId);
         const data = await response.json();
+        data.submittedQuestions = questionsArray; // Add the submitted questions 
         this.setState({ polls: data, loading: false });
     }
 }
